a804847126008ded10c5de9044ccfe2548c44024
chris <chrisler.laid@gmail.com>
2014-02-26 11:37:54 +0800

    Finish Chapter 9

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
-    self.current_user = user
+    self.current_user = user 
   end
 
   def signed_in?
@@ -16,9 +16,22 @@ module SessionsHelper
   	@current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
   end
 
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define  do
   factory :user do
-    name "Chrisler Laid"
-    email "chrisler.laid@gmail.com"
-    password "burgers"
-    password_confirmation "burgers"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+	password "foobar"
+	password confirmation "foobar"
+  end
+  
+  factory :admin do
+  	admin true
   end
 end
